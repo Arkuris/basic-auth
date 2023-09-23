835cdb62dbfdab0f3f35d0d24a471dd1868fb846
David Danilchik <davdd@mac.com>
2023-09-23 05:59:56 -0700

    Got my auth server to WORK LESGOOOOO

--- server.test.js
+++ server.test.js
@@ -6,12 +6,42 @@
 // afterEach(); // drop tables / delete records for records that get created between tests
 // afterAll(); //drop the tables in the DB
 
+const { app } = require('./src/server.js'); 
+const supertest = require('supertest');
+const request = supertest(app);
+
 describe('Testing our auth server', () => {
-  test('User should be able to create an account', () => {
+  let userInfo = {
+    username: 'testuser',
+    password: 'testpass',
+  };
 
+  test('User should be able to create an account', async () => {
+    const response = await request.post('/signup').send(userInfo);
+    expect(response.status).toBe(201);
+    expect(response.body.username).toBe(userInfo.username);
+    expect(response.body.password).toBeUndefined();
   });
 
-  test('User should be able to login to an existing account', () => {
+  test('User should be able to login to an existing account', async () => {
+    const response = await request.post('/signin')
+      .auth(userInfo.username, userInfo.password);
 
+    expect(response.status).toBe(200); 
+    expect(response.body.username).toBe(userInfo.username);
+    expect(response.body.password).toBeUndefined();
   });
-})
+});
+
+describe('Middleware Authentication', () => {
+  test('It should send error for a missing auth header', async () => {
+    const response = await request.post('/signin');
+    expect(response.status).toBe(403);
+  });
+
+  test('It should send error for wrong user credentials', async () => {
+    const response = await request.post('/signin')
+      .auth('wronguser', 'wrongpass');
+    expect(response.status).toBe(403);
+  });
+});
